90c57e4bce6af8fd210119aa6c17eaf055ec6361
paxorus <prakhar@brandeis.edu>
2016-08-29 23:00:31 -0400

    Zoomer class
    Continuation of refactoring.

--- zoomify/antroz.js
+++ zoomify/antroz.js
@@ -5,252 +5,81 @@ Paint utilizes correctOrigin to quickly give a blurry background and then drawIm
 
 */
 
-var Vector,LoadedImage;// library.js
-
+// CONSTANTS
 var MAX_ZOOM = 2;
+var FRAMES = 20;
+var MSPF = 15;
 
+var zoomRange = document.getElementById("zoomRange");
+var raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame || function(f) {setTimeout(f, MSPF)};
+
+// VARIABLES
+zoomRange.max = MAX_ZOOM;
+
+// initialize objects
 var canvas = new Canvas();
 var imageManager = new ImageManager();
-var zoomRange = document.getElementById("zoomRange");
-var body = document.getElementsByTagName("body")[0];
-
-var raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(f) {setTimeout(f,15)};
-var zoomLevel = 1;
-var FRAMES = 20;
-var o = new Vector(0,0);// origin, top-left
-var oldo = new Vector(0,0);// helper variable for o
-var start = new Vector(0,0);// helper variable for o
-var PANEL = new Vector(400,300);// base image dimensions
-var panel = new Vector(PANEL.x,PANEL.y);// during zooming 400x300-->800x600
-var blur = new Vector(PANEL.x,PANEL.y);// image dimensions
-var VIEW = new Vector(400,300);// canvas dimensions
-var finalPoint;
-var point = new Vector(0,0);// for zoomToPoint
-var zooming = false;// true while zooming effect
-// prepare zoomRange
-zoomRange.max = MAX_ZOOM;
-zoomRange.value = zoomLevel;
-// prepare blurry
+var zoomer = new Zoomer();
 var blurry = imageManager.getBlurry();
 
 
+// CONTROLLERS
 
-
-// EVENT LISTENERS
-// call zoomToPoint on double-click
+// zoom in on double-click
 var canvasElem = canvas.element();
 canvasElem.addEventListener("dblclick", function(evt) {
-    if(zoomLevel >= MAX_ZOOM){
+    if(zoomer.depth() >= MAX_ZOOM){
         return;
     }
-    zoomRange.value = zoomLevel + 1;
-    point.set(evt.clientX - canvasElem.offsetLeft, evt.clientY - canvasElem.offsetTop);
-    zoomToPoint(point);
+    zoomRange.value = zoomer.depth() + 1;
+    var point = new Vector(evt.clientX - canvasElem.offsetLeft, evt.clientY - canvasElem.offsetTop);
+    zoomer.zoomInTo(point);
 });
 
+// start translation on mouse down
 canvasElem.addEventListener("mousedown", function(evt) {
     evt.preventDefault();
     canvasElem.className = "closedHand";
-    start.set(evt.clientX, evt.clientY);
+    zoomer.startTranslate(evt.clientX, evt.clientY);
     document.addEventListener("mousemove", translate);
 });
 
+// end translation on mouse up
 document.addEventListener("mouseup", function(evt) {
+    evt.preventDefault();
     canvasElem.className = "openHand";
-    oldo.set(o.x, o.y);
+    zoomer.endTranslate();
     document.removeEventListener("mousemove", translate);
 });
 
 // called on mousemove, listener life from mousedown to mouseup
-function translate(evt){
-    o.set(evt.clientX - start.x + oldo.x, evt.clientY - start.y + oldo.y);
-    paint();
-    evt.stopPropagation();
+function translate(evt) {
+    zoomer.translate(evt.clientX, evt.clientY);
 }
 
+// change zoom depth to match range
 zoomRange.addEventListener("change", function(evt){
-    zoomLevel = parseInt(evt.target.value,10);
-    paint();
+    var desiredDepth = parseInt(evt.target.value,10);
+    zoomer.setDepth(desiredDepth);
 });
 
-// '+'
+// zoom in when '+' clicked
 var zoomInButton = document.getElementById("zoomInButton");
 zoomInButton.addEventListener("click", function (evt) {
-    if (zoomLevel >= MAX_ZOOM || zooming) {
+    if (zoomer.depth() >= MAX_ZOOM || zoomer.busy()) {
         return;
     }
-    zoomRange.value=zoomLevel + 1;
-    point.set(VIEW.x / 2, VIEW.y / 2);
-    zoomToPoint();
+    zoomRange.value = zoomer.depth() + 1;
+    zoomer.zoomInTo(canvas.center());
 });
 
-// '-'
+// zoom out when '-' clicked
 var zoomOutButton = document.getElementById("zoomOutButton");
 zoomOutButton.addEventListener("click", function (evt) {
-    if (zoomLevel <= 0 || zooming) {
+    if (zoomer.depth() <= 0 || zoomer.busy()) {
         return;
     }
-    zoomRange.value = zoomLevel - 1;
-    point.set(VIEW.x / 2, VIEW.y / 2);
-    zoomFromPoint();
+    zoomRange.value = zoomer.depth() - 1;
+    zoomer.zoomOutFrom(canvas.center());
 });
-
-
-
-
-
-
-// ensure that view is not out of bounds
-function correctOrigin(){
-    var blurSize=Math.pow(2,zoomLevel);
-    if(zooming){// reassign
-        blurSize=(zoomLevel>baseZoomLevel)?Math.pow(2,baseZoomLevel)*(1+df):Math.pow(2,baseZoomLevel)*(1-df/2);
-    }
-    
-    blur.set(blurSize*PANEL.x,blurSize*PANEL.y);
-    // console.log("Before: "+o.print());
-    o.set(Math.min(o.x,0),Math.min(o.y,0));// origin maxes out at (0,0)
-    // console.log("After: "+o.print());
-    var generic=(zooming)?panel:PANEL;
-    o.set(Math.max(o.x,VIEW.x-blur.x),Math.max(o.y,VIEW.y-blur.y));// origin mins...
-
-    // zoomLevel =0:0, =1:-400, =2:-1200 =n:400(1-2^n)
-
-}
-
-
-
-
-
-
-
- 
-// ZOOMING
-var f;// fatness, goes 0-->100
-var df;// goes 0-->1
-var baseZoomLevel;
-function zoomToPoint(){// triggered by dblclick or '+'
-    if(zooming){
-        return;
-    }
-    zooming=true;
-    oldo.set(o.x,o.y);
-    finalPoint=new Vector(2*(oldo.x-point.x)+VIEW.x/2,2*(oldo.y-point.y)+VIEW.y/2);
-    // console.log("DBLCLICK: "+oldo.print()+"-->"+finalPoint.print());
-    baseZoomLevel=zoomLevel;
-    f=0;
-    df=0;
-    raf(fatten);
-}
-function zoomFromPoint(){// triggered by '-'
-    if(zooming){
-        return;
-    }
-
-    zooming=true;
-    oldo.set(o.x,o.y);
-    finalPoint=new Vector(oldo.x/2-VIEW.x/4+point.x,oldo.y/2-VIEW.y/4+point.y);
-    // finalPoint=new Vector(3/4*oldo.x+point.x/2,3/4*oldo.y+point.y/2);
-    // console.log("oldo: "+oldo.print());
-    // console.log("finalPoint: "+finalPoint.print());
-    baseZoomLevel=zoomLevel;
-    f=0;
-    df=0;
-    raf(fatten2);
-}
-
-function fatten(){// recursive
-    df=f/FRAMES;// f/100
-    o.x=Math.round((finalPoint.x-oldo.x)*df+oldo.x);
-    o.y=Math.round((finalPoint.y-oldo.y)*df+oldo.y);
-    zoomLevel=baseZoomLevel+df;
-    panel.set((1+df)*PANEL.x,(1+df)*PANEL.y);
-    paint();
-
-    if(f<FRAMES){
-        f++;
-        raf(fatten);
-    }else{
-        console.log("DONE: "+o.print());
-        zooming=false;
-        baseZoomLevel=zoomLevel;
-        panel.set(PANEL.x,PANEL.y);
-        oldo.set(o.x,o.y);
-        paint();
-    }
-}
-
-function fatten2(){// recursive
-    df=f/FRAMES;// f/100
-    o.x=Math.round((finalPoint.x-oldo.x)*df+oldo.x);
-    o.y=Math.round((finalPoint.y-oldo.y)*df+oldo.y);
-    zoomLevel=baseZoomLevel-df;
-    panel.set((1-df/2)*PANEL.x,(1-df/2)*PANEL.y);
-    paint();
-
-    // console.log("Full size: "+blur.print());
-    if(f<FRAMES){
-        f++;
-        raf(fatten2);
-    }else{
-        // console.log("DONE: "+o.print());
-        zooming=false;
-        baseZoomLevel=zoomLevel;
-        panel.set(PANEL.x,PANEL.y);
-        oldo.set(o.x,o.y);
-        paint();
-    }
-}
-
-/*function skip(){
-    zoomLevel++;
-    o.x=Math.round(2*oldo.x-2*point.x+VIEW.x/2);
-    o.y=Math.round(2*oldo.y-2*point.y+VIEW.y/2);
-    oldo.set(o.x,o.y);
-    paint();
-}*/
-
-
-
-
-
-
-
-
-// rendering core: for panning and zooming
-// assume each image to be unsafe, calculate left and right limits,
-// drawImage and mark safe if in bounds, delete all unsafe images
-function paint(){
-    correctOrigin();
-    canvas.clear();
-    canvas.draw(blurry, o, blur);
-
-    // reset all images as unused
-    imageManager.allUnsafe();
-
-
-    
-    var generic=(zooming)?panel:PANEL;
-    // draw relevant images, mark them as used
-    var left=new Vector(Math.floor(-o.x/generic.x),Math.floor(-o.y/generic.y));
-    var right=new Vector(Math.ceil((-o.x+VIEW.x)/generic.x)-1,Math.ceil((-o.y+VIEW.y)/generic.y)-1);
-    
-
-    for(var i=left.x;i<=right.x;i++){// across x-axis
-        for(var j=left.y;j<=right.y;j++){// across y-axis
-            imageManager.load(baseZoomLevel, i, j);
-        }
-    }
-    
-    imageManager.clean();
-}
-
-
-
-
-
-
-
-
-// print all images in loaded[]
-function p(){var str="";for(var each in loaded){str+="\n"+loaded[each].name;}console.log(str.substring(1));}
--- zoomify/library.js
+++ zoomify/library.js
@@ -1,7 +1,6 @@
-var loadedNames,con,o,PANEL,panel,zooming,MAX_ZOOM,paint;// antroz.js
+var Vector, Canvas, LoadedImage, ImageManager, Zoomer;
 
-
-var Vector, LoadedImage, Canvas, ImageManager;
+// MODELS
 
 function Vector(xarg, yarg){
     this.set = function (x, y){
@@ -17,65 +16,29 @@ function Vector(xarg, yarg){
 
 function LoadedImage(i, j, name){
 
-    this.name = name;
-    this.safe = true;
-    this.elem = document.createElement("img");
-    this.elem.src = this.name;
+    this.constructor = function () {
+        this.name = name;
+        this.safe = true;
+        this.elem = document.createElement("img");
+        this.elem.src = this.name;
 
-    // this.elem.onload=function(){
-    //     if(zooming){
-    //         var blah = new Vector(o.x + panel.x * i, o.y + panel.y * j);
-    //         canvas.draw(this, blah, panel);
-    //     }else{
-    //         var blah = new Vector(o.x + PANEL.x * i, o.y + PANEL.y * j);
-    //         canvas.draw(this, blah, PANEL);
-    //     }
-    // };
-    var that = this;
-    this.elem.onload = function () {
-        that.draw();
-    };
-
-    this.print=function(){
-        return "{name:" + this.name + ",safe:" + this.safe + "}";
-    };
+        var that = this;
+        this.elem.onload = function () {
+            that.draw();
+        };
+    }
 
     this.draw = function () {
-        if (zooming) {
-            var blah = new Vector(o.x + panel.x * i, o.y + panel.y * j);
-            canvas.draw(this.elem, blah, panel);
-        } else {
-            var blah = new Vector(o.x + PANEL.x * i, o.y + PANEL.y * j);
-            canvas.draw(this.elem, blah, PANEL);
-        }
-    }
-}
-
-function Canvas() {
-    var element, context;
-
-    this.constructor = function () {
-        element = document.getElementById("canvas");
-        context = element.getContext("2d");
-        this.resize();
+        this.safe = true;
+        var panel = zoomer.panel();
+        var origin = zoomer.origin();
+        var coordinates = new Vector(origin.x + panel.x * i, origin.y + panel.y * j);
+        canvas.draw(this.elem, coordinates, panel);
     }
 
-    this.resize = function () {
-        element.style.left = (window.innerWidth - element.width) / 2 + "px";
-        element.style.top = (window.innerHeight - element.height) / 2+ "px";
-    }
-
-    this.clear = function () {
-        element.width = element.width;
-    }
-
-    this.draw = function (blurry, o, blur) {
-        context.drawImage(blurry, o.x, o.y, blur.x, blur.y);
-    }
-
-    this.element = function () {
-        return element;
-    }
+    this.toString=function(){
+        return "{name:" + this.name + ", safe:" + this.safe + "}";
+    };
 
     this.constructor();
 }
@@ -89,6 +52,7 @@ function ImageManager(){
         loadedNames = [];
     }
 
+    // generate all possible image names from 0 to MAX_ZOOM
     this.populateImageNames = function() {
         imageNames = [];
         // from a to c's, 0,1,2
@@ -106,14 +70,14 @@ function ImageManager(){
     }
 
     this.allUnsafe = function () {
-        for(var each in loaded){
+        for (var each in loaded) {
            loaded[each].safe = false;
         }
     }
 
     this.clean = function () {
         // remove unused images (images marked as set to safe)
-        for (var i = loaded.length-1; i >=0; i--) {
+        for (var i = loaded.length - 1; i >= 0; i --) {
             if (!loaded[i].safe) {// if unsafe
                 var removed = loaded.splice(i, 1)[0].name;
                 loadedNames.splice(i, 1);
@@ -123,11 +87,10 @@ function ImageManager(){
 
     this.getBlurry = function () {
         var blurry = document.createElement("img");
-        console.log(imageNames);
-        console.log(loaded);
-
         blurry.src = imageNames[0][0];
-        blurry.onload = paint;
+        blurry.onload = function () {
+            zoomer.update();
+        }
         return blurry;
     }
 
@@ -140,10 +103,14 @@ function ImageManager(){
             loaded.push(new LoadedImage(i, j, name, callback));
             loadedNames.push(name);
         } else {
+            loaded[index].safe = true;
             loaded[index].draw();
         }
     }
 
+    this._dump = function () {
+        return loadedNames;
+    }
 
     this.constructor();
 }
@@ -152,20 +119,231 @@ function ImageManager(){
 
 
 
+function Zoomer(){
+    var origin = new Vector(0, 0);// origin, top-left
+    var oldOrigin = new Vector(0, 0);// helper variable for origin
+    var start = new Vector(0, 0);// helper variable for origin
+    var PANEL = new Vector(400, 300);// base image dimensions
+    var panel = new Vector(PANEL.x, PANEL.y);// during zooming 400x300-->800x600
+    var blur = new Vector(PANEL.x, PANEL.y);// image dimensions
+    var depth = 1;
+    var f;// fatness, goes 0-->100
+    var df;// goes 0-->1
+    var baseDepth = depth;
+    var that = this;
+
+    var finalPoint;
+    var zooming = false;// true while zooming effect
+    zoomRange.value = depth;
 
 
+    // ensure that canvas.dim() is not out of bounds
+    this.correctOrigin = function() {
+        var blurSize = Math.pow(2, depth);
+        if (zooming) {// reassign
+            blurSize = (depth > baseDepth) ? Math.pow(2, baseDepth) * (1 + df) : Math.pow(2, baseDepth) * (1 - df / 2);
+        }
+        
+        blur.set(blurSize * PANEL.x, blurSize * PANEL.y);
+        origin.set(Math.min(origin.x, 0), Math.min(origin.y, 0));// origin maxes out at (0,0)
+        origin.set(Math.max(origin.x, canvas.dim().x - blur.x), Math.max(origin.y, canvas.dim().y - blur.y));// origin mins...
 
-
-
-
-
-
-
-
-
-/*function onRepeat(){// recursive, calls paint
-    paint();
-    if(!terminate){
-        raf(onRepeat);
+        // depth =0:0, =1:-400, =2:-1200 =n:400(1-2^n)
     }
-}*/
+
+
+
+    this.zoomInTo = function(point) {// triggered by dblclick or '+'
+        if (zooming) {
+            return;
+        }
+        zooming = true;
+        oldOrigin.set(origin.x, origin.y);
+        finalPoint = new Vector(2 * (oldOrigin.x - point.x) + canvas.dim().x / 2, 2 * (oldOrigin.y - point.y) + canvas.dim().y / 2);
+        baseDepth = depth;
+        f = 0;
+        df = 0;
+        raf(function () {
+            that.fatten();
+        });
+    }
+
+    this.zoomOutFrom = function(point) {// triggered by '-'
+        if (zooming) {
+            return;
+        }
+
+        zooming = true;
+        oldOrigin.set(origin.x, origin.y);
+        finalPoint = new Vector(oldOrigin.x / 2 - canvas.dim().x / 4 + point.x, oldOrigin.y / 2 - canvas.dim().y / 4 + point.y);
+        baseDepth = depth;
+        f = 0;
+        df = 0;
+        raf(function () {
+            that.fatten2();
+        });
+    }
+
+    this.fatten = function () {// recursive
+        df = f / FRAMES;// f/100
+        origin.x = Math.round((finalPoint.x - oldOrigin.x) * df + oldOrigin.x);
+        origin.y = Math.round((finalPoint.y - oldOrigin.y) * df + oldOrigin.y);
+        depth = baseDepth + df;
+        panel.set((1 + df) * PANEL.x, (1 + df) * PANEL.y);
+        this.update();
+
+        if(f < FRAMES){
+            f ++;
+            raf(function () {
+                that.fatten();
+            });
+        }else{
+            zooming = false;
+            baseDepth = depth;
+            panel.set(PANEL.x, PANEL.y);
+            oldOrigin.set(origin.x, origin.y);
+            this.update();
+        }
+    }
+
+    this.fatten2 = function () {// recursive
+        df = f / FRAMES;// f/100
+        origin.x = Math.round((finalPoint.x - oldOrigin.x) * df + oldOrigin.x);
+        origin.y = Math.round((finalPoint.y - oldOrigin.y) * df + oldOrigin.y);
+        depth = baseDepth - df;
+        panel.set((1 - df / 2) * PANEL.x, (1 - df / 2) * PANEL.y);
+        this.update();
+
+        if (f < FRAMES) {
+            f ++;
+            raf(function () {
+                that.fatten2();
+            });
+        } else {
+            zooming = false;
+            baseDepth = depth;
+            panel.set(PANEL.x, PANEL.y);
+            oldOrigin.set(origin.x, origin.y);
+            this.update();
+        }
+    }
+
+    // rendering core: for panning and zooming
+    // assume each image to be unsafe, calculate left and right limits,
+    // drawImage and mark safe if in bounds, delete all unsafe images
+    this.update = function () {
+        this.correctOrigin();
+        canvas.clear();
+        canvas.draw(blurry, origin, blur);
+
+        // reset all images as unused
+        imageManager.allUnsafe();
+        
+        // draw relevant images, mark them as used
+        var left = _getLeft();
+        var right = _getRight();
+
+        for(var i = left.x; i <= right.x; i ++){// across x-axis
+            for(var j = left.y; j <= right.y; j ++){// across y-axis
+                imageManager.load(baseDepth, i, j);
+            }
+        }
+        
+        imageManager.clean();
+    }
+
+    this.origin = function () {
+        return origin;
+    }
+
+    this.setOrigin = function (x, y) {
+        origin.set(x, y);
+        this.update();
+    }
+
+    this.setDepth = function (z) {
+        depth = z;
+        this.update();
+    }
+
+    this.depth = function () {
+        return depth;
+    }
+
+    this.startTranslate = function (x, y) {
+        start = new Vector(x, y);
+    }
+
+    this.endTranslate = function () {
+        oldOrigin.set(origin.x, origin.y);
+    }
+
+    this.translate = function (x, y) {
+        zoomer.setOrigin(x - start.x + oldOrigin.x, y - start.y + oldOrigin.y);
+    }
+
+    this.panel = function () {
+        return panel;
+    }
+
+    this.busy = function () {
+        return zooming;
+    }
+
+    function _getLeft() {
+        var x = Math.floor(-origin.x / panel.x);
+        var y = Math.floor(-origin.y / panel.y);
+        return new Vector(x, y);
+    }
+
+    function _getRight() {
+        var x = Math.ceil((-origin.x + canvas.dim().x) / panel.x) - 1;
+        var y = Math.ceil((-origin.y + canvas.dim().y) / panel.y) - 1;
+        return new Vector(x, y);
+    }
+}
+
+
+// VIEW
+
+function Canvas() {
+    var element, context;
+
+    // initial resize
+    this.constructor = function () {
+        element = document.getElementById("canvas");
+        context = element.getContext("2d");
+        this.resize();
+    }
+
+    // center canvas on page
+    this.resize = function () {
+        element.style.left = (window.innerWidth - element.width) / 2 + "px";
+        element.style.top = (window.innerHeight - element.height) / 2+ "px";
+    }
+
+    // clear the canvas
+    this.clear = function () {
+        element.width = element.width;
+    }
+
+    // 
+    this.draw = function (image, position, dimensions) {
+        context.drawImage(image, position.x, position.y, dimensions.x, dimensions.y);
+    }
+
+    // get the DOM element 
+    this.element = function () {
+        return element;
+    }
+
+    this.dim = function () {
+        return new Vector(element.width, element.height);
+    }
+
+    this.center = function () {
+        return new Vector(element.width / 2, element.height / 2);
+    }
+
+    this.constructor();
+}
